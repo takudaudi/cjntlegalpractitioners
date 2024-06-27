a43c1753287544ba0b75726b7f0421d4402f8256
Fred P. Majoni <fred@trafficsafety.co.zw>
2024-06-27 08:50:00 +0200

    try

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://doktor.akselinc.com',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
